604bd1d8d3f787ace902322be44ca4edb99b96ad
SaugatMgr <saugatmagar433@gmail.com>
2023-09-13 17:25:35 +0545

    moved sensitive data to env file

--- config/settings.py
+++ config/settings.py
@@ -170,5 +170,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'securesally@gmail.com'
-EMAIL_HOST_PASSWORD = 'rcsdwfcrozhvpzmr'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
