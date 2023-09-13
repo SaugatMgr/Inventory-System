47e3c875b1e46a9aecf296c6139b3b5cc31ccdfe
SaugatMgr <saugatmagar433@gmail.com>
2023-09-13 17:17:13 +0545

    deleted permissions from apps and placed in utils

--- apps/accounts/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class SupplierPermission(BasePermission):
-    def has_permission(self, request):
-        if request.user.role == "Supplier":
-            return True
-        return False
