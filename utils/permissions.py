cf65caf08cf3abd194a74730c179f07093d88788
SaugatMgr <saugatmagar433@gmail.com>
2023-09-15 12:03:16 +0545

    edited supplier permission

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,7 +1,17 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import (
+    BasePermission,
+)
 
-class SupplierPermission(BasePermission):
+
+class IsSupplierPermission(BasePermission):
+    edit_methods = ("POST", "PUT", "PATCH", "DELETE")
+
     def has_permission(self, request, view):
-        if request.user.role == "Supplier":
+        if request.user.role == "Supplier" or request.user.is_superuser:
             return True
         return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        return request.user == obj.user and request.method in self.edit_methods
