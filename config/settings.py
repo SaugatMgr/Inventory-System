3e8afb5b3d48963f605e8d9efcc97ede19a4953a
SaugatMgr <saugatmagar433@gmail.com>
2023-09-15 12:32:22 +0545

    added custom middleware

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "apps.accounts.middlewares.CustomMiddleware",
+    "utils.threads.RequestMiddleware",
 ]
 
 REST_FRAMEWORK = {
