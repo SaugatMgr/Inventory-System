e5ba7c21344f2fcf527100f9a466420e61b326b6
SaugatMgr <saugatmagar433@gmail.com>
2023-09-19 12:42:24 +0545

    new choices for added models

--- apps/products/constant.py
+++ apps/products/constant.py
@@ -1,23 +1,40 @@
 PRODUCT_TYPE_CHOICES = [
-        ("Food","Food"),
-        ("Clothing","Clothing"),
-        ("Electronics","Electronics"),
-        ("Accessories","Accessories"),
-        ("Shoes","Shoes"),
-    ]
+    ("Food", "Food"),
+    ("Clothing", "Clothing"),
+    ("Electronics", "Electronics"),
+    ("Accessories", "Accessories"),
+    ("Shoes", "Shoes"),
+]
 PRODUCT_TAX = [
-    ('10','Vat @10%'),
-    ('9','Vat @9%'),
-    ('11','Vat @12%'),
-    ('13','Vat @13%'),
+    ('10', 'Vat @10%'),
+    ('9', 'Vat @9%'),
+    ('11', 'Vat @12%'),
+    ('13', 'Vat @13%'),
 ]
 TAX_METHOD = [
-    ('Exclusive','Exclusive'),
-    ('Non-Exclusive','Non-Exclusive'),
+    ('Exclusive', 'Exclusive'),
+    ('Non-Exclusive', 'Non-Exclusive'),
 ]
 
 BARCODE_PAPER_SIZE = [
-    ('50','50 mm (1.95 Inch)'),
-    ('40','40 mm (1.65 Inch)'),
-    ('30','30 mm (1.35 Inch)'),
+    ('50', '50 mm (1.95 Inch)'),
+    ('40', '40 mm (1.65 Inch)'),
+    ('30', '30 mm (1.35 Inch)'),
+]
+SALE_STATUS = [
+    ('Complete', 'Complete'),
+    ('Incomplete', 'Incomplete'),
+    ('Drafts', 'Drafts')
+]
+ORDER_TAX = [
+    ('8', 'Vat @8%'),
+    ('9', 'Vat @9%'),
+    ('10', 'Vat @10%'),
+    ('11', 'Vat @11%'),
+    ('12', 'Vat @12%'),
+    ('13', 'Vat @13%'),
+]
+ADJUSTMENT_TYPE = [
+    ('Addition', 'Addition'),
+    ('Substraction', 'Substraction')
 ]
