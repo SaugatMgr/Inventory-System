2ed4481c8aa5dbbb1e8f2468101898e664a0dce1
SaugatMgr <saugatmagar433@gmail.com>
2023-09-19 09:05:59 +0545

    views for products app

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,138 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from apps.products.models import (
+    Brand,
+    Category,
+    SubCategory,
+    Product,
+    Unit,
+    Warehouse,
+)
+from apps.products.serializers import (
+    BrandSerializers,
+    CategorySerializer,
+    ProductSerializer,
+    GETProductSerializer,
+    UnitSerializer,
+    GetCategorySeralizer,
+    GetUnitSeralizer,
+    GetBrandSeralizer,
+    WarehouseSerializer,
+    SubCategorySerializer,
+)
+from apps.accounts.pagination import MyPagination
+
+from rest_framework.permissions import (
+    AllowAny,
+    IsAdminUser,
+    IsAuthenticated,
+)
+
+
+class CommonModelViewSet(ModelViewSet):
+    pagination_class = MyPagination
+
+
+class BrandViewSet(CommonModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializers
+    http_method_names = ["get", "post", "put", "delete"]
+    filterset_fields = ["brand_name"]
+
+    def create(self, request, *args, **kwargs):
+        serializers = BrandSerializers(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save(created_by=request.user, modified_by=None)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        serializers = BrandSerializers(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save(modified_by=request.user)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return GetBrandSeralizer
+        return super().get_serializer_class()
+
+
+class SubCategoryViewSet(CommonModelViewSet):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+    
+
+class CategoryViewSet(CommonModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        serializers = CategorySerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save(created_by=request.user, modified_by=None)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return GetCategorySeralizer
+        return super().get_serializer_class()
+
+
+class UnitViewSet(ModelViewSet):
+    queryset = Unit.objects.all()
+    serializer_class = UnitSerializer
+    http_method_names = ["get", "post", "put", "patch", "delete"]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["=short_name"]
+    ordering_fields = ["short_name"]
+
+    def create(self, request, *args, **kwargs):
+        serializers = UnitSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save(created_by=request.user)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return GetUnitSeralizer
+        return super().get_serializer_class()
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    http_method_names = ["get", "post", "put", "patch", "delete"]
+    filterset_fields = ["created_by"]
+    permission_classes_by_action = {
+        "list": [AllowAny],
+        "retrieve": [IsAuthenticated],
+        "create": [IsAdminUser],
+        "update": [IsAuthenticated],
+    }
+
+    def get_permissions(self):
+        try:
+            return [
+                permission()
+                for permission in self.permission_classes_by_action[self.action]
+            ]
+        except:
+            return [permission() for permission in self.permission_classes]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return GETProductSerializer
+        return super().get_serializer_class()
+
+
+class WarehouseViewset(ModelViewSet):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
+
+
+class BarcodeViewset(ModelViewSet):
+    pass
