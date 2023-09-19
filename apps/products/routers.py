885372c4901ef67889a7861388b8bd5ba25338a2
SaugatMgr <saugatmagar433@gmail.com>
2023-09-19 12:43:22 +0545

    new api endpoints for added views

--- apps/products/routers.py
+++ apps/products/routers.py
@@ -5,6 +5,11 @@ from apps.products.views import (
     CategoryViewSet,
     SubCategoryViewSet,
     UnitViewSet,
+    BarcodeViewSet,
+    PurchaseViewSet,
+    SalesViewSet,
+    PurchaseInvoiceViewSet,
+    AdjustmentViewset,
 )
 
 router = DefaultRouter()
@@ -13,3 +18,8 @@ router.register("brands", BrandViewSet)
 router.register("categories", CategoryViewSet)
 router.register("sub-categories", SubCategoryViewSet)
 router.register("units", UnitViewSet)
+router.register("barcode", BarcodeViewSet)
+router.register("purchase", PurchaseViewSet)
+router.register("sales", SalesViewSet)
+router.register("purchase-invoice", PurchaseInvoiceViewSet)
+router.register("adjustment", AdjustmentViewset)
