67fab3d03952a4542226d52597b99cd32a4f37b9
SaugatMgr <saugatmagar433@gmail.com>
2023-09-20 21:53:33 +0545

    moved forgot password and reset password to user view set and moved generate otp function to local utils file

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.decorators import action
-import pyotp
 
 from rest_framework.permissions import (
     AllowAny,
@@ -40,6 +39,8 @@ from apps.accounts.serializers import (
     ForgotPasswordSerializer,
 )
 from apps.accounts.pagination import MyPagination
+from apps.accounts.utils import generate_otp
+
 from utils.permissions import (
     IsSupplierPermission,
 )
@@ -78,17 +79,21 @@ class UserViewSet(CommonModelViewset):
         #     return GetUserListSerializer
         if self.action == "change_password":
             return ChangePasswordSerializer
+        elif self.action == "forgot_password":
+            return PasswordResetRequestSerializer
+        elif self.action == "reset_password":
+            return ForgotPasswordSerializer
         return super().get_serializer_class()
 
     @action(permission_classes=[IsAuthenticated],
             methods=['post'], url_path='change-password', detail=True)
     def change_password(self, request, pk=None):
-        user = self.request.user
+        user = request.user
         serializer = ChangePasswordSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        password = serializer.validated_data["password"]
         old_password = serializer.validated_data["old_password"]
+        password = serializer.validated_data["password"]
 
         if not request.user.check_password(old_password):
             return Response({"message": "Old password is not correct"})
@@ -98,6 +103,41 @@ class UserViewSet(CommonModelViewset):
             user.save()
 
         return Response({"success": ["Password reset successfully"]}, status=status.HTTP_200_OK)
+    
+    @action(methods=['post'], detail=False, url_path='forgot-password', permission_classes=[AllowAny])
+    
+    def forgot_password(self, request, pk=None):
+        email_serializer = PasswordResetRequestSerializer(data=request.data)
+        email_serializer.is_valid(raise_exception=True)
+        email = email_serializer.validated_data['email']
+
+        # getting the user after validating email
+        user = get_object_or_404(User, email=email)
+        otp = generate_otp()
+        user.otp = otp
+        user.save()
+
+        reset_password_url = f"http://127.0.0.1:8000/api/forgot-password/{user.id}/reset-password/"
+
+        send_otp_email(user.email, reset_password_url, otp)
+
+        return Response({"success": "Email has been sent successfully", "data": {"email": user.id}}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['post'], detail=True, url_path='reset-password', permission_classes=[AllowAny])
+    def reset_password(self, request, pk=None):
+
+        user = get_object_or_404(User, id=pk)
+
+        forgot_password_serializer = ForgotPasswordSerializer(
+            data=request.data, context={'user': user})
+        forgot_password_serializer.is_valid(raise_exception=True)
+        password = forgot_password_serializer.validated_data['password']
+
+        user.set_password(password)
+        user.save()
+
+        # return success response after updating the password
+        return Response({"success": "Password reset successful"}, status=status.HTTP_202_ACCEPTED)
 
     def update(self, instance, validated_data):
 
@@ -230,11 +270,6 @@ class WarehouseViewSet(CommonModelViewset):
             return [permission() for permission in self.permission_classes]
 
 
-def generate_otp():
-    secret_key = pyotp.random_base32()
-    otp = pyotp.TOTP(secret_key)
-
-    return otp.now()
 
 
 class ForgotPasswordViewSet(ModelViewSet):
@@ -247,40 +282,6 @@ class ForgotPasswordViewSet(ModelViewSet):
             return ForgotPasswordSerializer
         return super().get_serializer_class()
 
-    def create(self, request):
-
-        # using email serializer to validate the user password
-        email_serializer = PasswordResetRequestSerializer(data=request.data)
-        email_serializer.is_valid(raise_exception=True)
-        email = email_serializer.validated_data['email']
-
-        # getting the user after validating email
-        user = get_object_or_404(User, email=email)
-        otp = generate_otp()
-        user.otp = otp
-        user.save()
-
-        reset_password_url = f"http://127.0.0.1:8000/api/forgot-password/{user.id}/reset-password/"
-
-        send_otp_email(user.email, reset_password_url, otp)
-
-        return Response({"success": "Email has been sent successfully", "data": {"email": user.id}}, status=status.HTTP_201_CREATED)
-
-    @action(methods=['POST'], detail=True, url_path='reset-password', permission_classes=[AllowAny])
-    def reset_password(self, request, pk=None):
-
-        user = get_object_or_404(User, id=pk)
-
-        forgot_password_serializer = ForgotPasswordSerializer(
-            data=request.data, context={'user': user})
-        forgot_password_serializer.is_valid(raise_exception=True)
-        password = forgot_password_serializer.validated_data['password']
-
-        user.set_password(password)
-        user.save()
-
-        # return success response after updating the password
-        return Response({"success": "Password reset successful"}, status=status.HTTP_202_ACCEPTED)
 
 
 class RegisterUserViewSet(generics.CreateAPIView):
