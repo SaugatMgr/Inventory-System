2f5945a3dbfb7903e93a01217246ef6535e0a3a1
SaugatMgr <saugatmagar433@gmail.com>
2023-10-04 08:51:48 +0545

    necessary model changes

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,7 @@
+import uuid
+import pyotp
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import uuid
 
 from apps.accounts.managers import CustomUserManager
 from apps.accounts.constant import (
@@ -8,9 +9,7 @@ from apps.accounts.constant import (
     ROLE_CHOICES,
     CUSTOMER_GROUP_CHOICES,
 )
-from apps.products.models import (
-    Warehouse
-)
+# from apps.products.models import Warehouse
 from utils.validation_for_phone_number import (
     validate_mobile_number,
     valid_emails,
@@ -21,7 +20,7 @@ from utils.models import (
 )
 
 
-class User(AbstractUser):
+class User(AbstractUser, Address):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     full_name = models.CharField(max_length=100)
     phone = models.CharField(
@@ -32,21 +31,23 @@ class User(AbstractUser):
         max_length=10,
         choices=GENDER_CHOICES,
     )
-    username = models.CharField(max_length=150,
-                                unique=True,)
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+    )
     role = models.CharField(
         max_length=20,
         choices=ROLE_CHOICES,
     )
 
-    otp = models.CharField(null=True, blank=True, max_length=6)
-    otp_used = models.BooleanField(default=False)
-
-    profile_image = models.ImageField(
-        upload_to="profile/", blank=True, null=True)
+    profile_image = models.ImageField(upload_to="profile/", blank=True, null=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["full_name", "username", "phone", ]
+    REQUIRED_FIELDS = [
+        "full_name",
+        "username",
+        "phone",
+    ]
 
     objects = CustomUserManager()
 
@@ -54,7 +55,7 @@ class User(AbstractUser):
         return self.email
 
 
-class Supplier(CommonInfo, Address):
+class Supplier(CommonInfo):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.CharField(max_length=100)
     supplier_code = models.CharField(max_length=50)
@@ -63,29 +64,45 @@ class Supplier(CommonInfo, Address):
         return self.user.full_name
 
 
-class Customer(CommonInfo, Address):
+class Customer(CommonInfo):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    supplier_name = models.ManyToManyField(
-        Supplier,
-        related_name="consumers"
-    )
-    customer_group = models.CharField(
-        max_length=10, choices=CUSTOMER_GROUP_CHOICES)
+    supplier_name = models.CharField(max_length=150)
+    customer_group = models.CharField(max_length=10, choices=CUSTOMER_GROUP_CHOICES)
     reward_point = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.created_by.full_name
+        return self.user.full_name
 
 
-class Biller(CommonInfo, Address):
+class Biller(CommonInfo):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     NID = models.CharField(max_length=13)
-    warehouse = models.ForeignKey(
-        Warehouse,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
+    # warehouse = models.ForeignKey(
+    #     Warehouse,
+    #     on_delete=models.SET_NULL,
+    #     null=True,
+    # )
     biller_code = models.CharField(max_length=50)
 
     def __str__(self):
         return self.user.full_name
+
+
+class OTP(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    secret_key = models.CharField(max_length=32)
+    otp_code = models.CharField(max_length=6)
+
+    @classmethod
+    def generate_otp(cls, user):
+        otp_instance, created = cls.objects.get_or_create(user=user)
+
+        secret_key = pyotp.random_base32()
+        otp = pyotp.TOTP(secret_key)
+        otp_code = otp.now()
+
+        otp_instance.secret_key = secret_key
+        otp_instance.otp_code = otp_code
+        otp_instance.save()
+
+        return otp_code
