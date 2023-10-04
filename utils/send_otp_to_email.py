b40e4e05897822d8490814c5841b60fc91d07c04
SaugatMgr <saugatmagar433@gmail.com>
2023-10-04 08:41:58 +0545

    removed reset password url from the function parameter

--- utils/send_otp_to_email.py
+++ utils/send_otp_to_email.py
@@ -2,11 +2,10 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 
-def send_otp_email(user_email, reset_password_url, otp):
+def send_otp_email(user_email, otp):
     subject = "Your OTP Code"
-    message = f"Your otp code is {otp}. Go to {reset_password_url} to reset your password"
+    message = f"Your otp code is {otp}."
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [user_email]
-    
-    
+
     send_mail(subject, message, from_email, recipient_list)
